f01f8baf88795353f9a331fa09af7a783a407896
NutsaJ <102469713+NutsaJ@users.noreply.github.com>
2022-06-06 14:16:26 +0400

    add

--- signin.js
+++ signin.js
@@ -2,3 +2,7 @@ document.querySelector('#signin-form').addEventListener("submit", function(event
      event.preventDefault();
    alert('logged in');
  })
+ 
+ function getRndInteger(min, max) {
+    return Math.floor(Math.random() * 5) + 1;
+  }
